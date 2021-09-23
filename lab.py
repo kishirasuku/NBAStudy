ac15fab35655db482e536a38da0ab202e86a7a3f
kishirasuku <kishimotorasuku@gmail.com>
2021-09-24 04:04:29 +0900

    add : compareRank

--- lab.py
+++ lab.py
@@ -5,8 +5,8 @@ import codecs
 import operator
 import time
 
-def readFile(fileName):
-    """[csvファイルからデータを取得してリストを返す]
+def readStatsFile(fileName):
+    """[csvファイルからスタッツデータを取得してスタッツリストを返す]
 
     Args:
         fileName ([String]): [ファイル名]
@@ -19,7 +19,21 @@ def readFile(fileName):
 
     return df.values.tolist()
 
-def calculateALL(T,W,dataList):
+def readRankFile(fileName):
+    """[csvファイルから順位データを取得して順位リストを返す]
+
+    Args:
+        fileName ([String]): [ファイル名]
+
+    Returns:
+        [list]: [各チームのデータ]
+    """
+    with codecs.open(fileName, "r", "Shift-JIS", "ignore") as file:
+        df = pd.read_table(file, delimiter=",")
+
+    return df.values.tolist()
+
+def calculateALL(T,dataList,rankList):
     """[全ての重みパターンで計算し、一番実際の順位に近いおもみリストを返す]
 
     Args:
@@ -30,8 +44,9 @@ def calculateALL(T,W,dataList):
     Returns:
         [list]: [実際の準備に一番近いおもみリスト]
     """
-    bestW = [W,10]
+    bestW = [None,10000]
 
+    #5^8通りの重みで試算
     for a in range(1,6):
         for b in range(1,6):
             for c in range(1,6):
@@ -42,16 +57,19 @@ def calculateALL(T,W,dataList):
                                 for h in range(1,6):
                                         W = [a,b,c,d,e,f,g,h]
 
+                                        #Tを作成
                                         arrangeData(T,W,dataList)
 
-                                        score = compareRanking(T)
-
-                                        if bestW[1] < score:
-                                            bestW[0] = W
-                                            bestW[1] = score
-
+                                        #3位までの評価点数結果を出力
                                         print3rdResult(T)
                                         print()
+
+                                        #Tと実際の順位を比較しスコアを計算(scoreが小さいほど順位が近い)
+                                        score = compareRanking(T,rankList)
+
+                                        if bestW[1] > score:
+                                            bestW[0] = W
+                                            bestW[1] = score
 
                                         T = []
 
@@ -80,10 +98,16 @@ def arrangeData(T,W,dataList):
             element[1] += W[j-1]*dataList[i][j]
         T.append(element)
 
-def compareRanking(T):
+def compareRanking(T,rankList):
+    #0からスタート、１チームごとの順位の離れ方で計算
     score = 0
+
+    for i,elm in enumerate(sorted(T,key=operator.itemgetter(1),reverse=True)):
+        for rank in rankList:
+            if elm[0] == rank[0]:
+                score += abs(i-rank[2])
+
     return score
-    
 
 def printResult(T):
     """[データからの全順位を出力]
@@ -106,23 +130,44 @@ def print3rdResult(T):
     for i,elm in enumerate(by3rd):
         print(i+1,"位",elm[0],":",elm[1],"点",flush=True)
 
+def printW(W):
+    print("FG%",W[0])
+    print("3P%",W[1])
+    print("FT%",W[2])
+    print("ORB",W[3])
+    print("DRB",W[4])
+    print("APG",W[5])
+    print("SPG",W[6])
+    print("BPG",W[7])
+
 
 df = None
 dataList = None
 T = []
-W = [5,5,5,5,5,5,5,5]
-fileName="2021レギュラーシーズン.csv"
+stats_file = "2021レギュラーシーズンスタッツ.csv"
+rank_file = "2021レギュラーシーズン順位.csv"
 
-dataList = readFile(fileName)
-        
+#データ読み込み
+statsDataList = readStatsFile(stats_file)
+rankDataList = readRankFile(rank_file)
+
 #arrangeData(T,W,dataList)
 
 #printResult(T)
 
+#試算開始
 start = time.time()
 
-bestW = calculateALL(T,W,dataList)
+bestW = calculateALL(T,statsDataList,rankDataList)
 
 end = time.time()
 
 print("Time : ",end-start)
+
+T = []
+
+arrangeData(T,bestW[0],statsDataList)
+
+printResult(T)
+
+printW(bestW[0])
